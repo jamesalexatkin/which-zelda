d6c86306c9e437124665acacdbb6ee2b64a1fef6
James Atkin <curlyw42@gmail.com>
2020-07-20 23:52:47 +0100

    Added console click

--- react-rewrite/src/Console.js
+++ react-rewrite/src/Console.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Console({name, img_src}) {
+
+    function handleClick() {
+        setOwned(!owned);
+    }
+
+    const [owned, setOwned] = useState(false);
+
     return (
-        <div className="card">
-            <img src={img_src}></img>
+        <div className="card" onClick={handleClick}>
+            <img className={owned ? "img" : "img-gray"} src={img_src}></img>
             <div className="container">
                 <h4><b>{name}</b></h4>
             </div>

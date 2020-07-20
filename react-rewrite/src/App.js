36982583657daa9bb5ab59db7c5541546f0c0e5e
James Atkin <curlyw42@gmail.com>
2020-07-20 23:33:14 +0100

    Added home console and game functionality

--- react-rewrite/src/App.js
+++ react-rewrite/src/App.js
@@ -2,7 +2,11 @@ import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Console from './Console.js';
+import Game from './Game.js';
 import handhelds from './data/handhelds.json';
+import home_consoles from './data/home_consoles.json';
+import main_series from './data/main_series.json';
+import spin_offs from './data/spin_offs.json';
 
 function App() {
 
@@ -17,6 +21,11 @@ function App() {
 
         <h3>Home consoles</h3>
 
+        <div class="row">
+          {home_consoles.map(home_console => (
+            <Console name={home_console.name} img_src={home_console.img_src}/>
+          ))}
+        </div>
 
         <h3>Handheld consoles</h3>        
 
@@ -35,7 +44,19 @@ function App() {
 
         <h3>Main series</h3>
 
+        <div class="row">
+          {main_series.map(game => (
+            <Game name={game.name} img_src={game.img_src}/>
+          ))}
+        </div>
+
         <h3>Spin-offs</h3>
+
+        <div class="row">
+          {spin_offs.map(game => (
+            <Game name={game.name} img_src={game.img_src}/>
+          ))}
+        </div>
 
       </div>
 

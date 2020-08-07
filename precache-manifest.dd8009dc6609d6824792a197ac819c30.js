d2495869c613b174c4a20c89868c4cdfb031cf71
James Atkin <curlyw42@gmail.com>
2020-08-07 19:07:35 +0100

    Updates

---
+++ precache-manifest.dd8009dc6609d6824792a197ac819c30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f813ac5f7d92636c290014613f52558d",
+    "url": "/which-zelda/index.html"
+  },
+  {
+    "revision": "edc7cc7bf448a043a42a",
+    "url": "/which-zelda/static/css/main.e39d5e6d.chunk.css"
+  },
+  {
+    "revision": "fd81425bcd1e4e481bb7",
+    "url": "/which-zelda/static/js/2.90a84ea0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/which-zelda/static/js/2.90a84ea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edc7cc7bf448a043a42a",
+    "url": "/which-zelda/static/js/main.9cd313cb.chunk.js"
+  },
+  {
+    "revision": "344349d09b2e88988a02",
+    "url": "/which-zelda/static/js/runtime-main.301ed116.js"
+  },
+  {
+    "revision": "a9519052e74204c34f6ee1ad6ba5b80a",
+    "url": "/which-zelda/static/media/HyliaSerifBeta-Regular.a9519052.otf"
+  }
+]);

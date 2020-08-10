68000e8d111c20b3f3705b0766d60599bc70488e
James Atkin <curlyw42@gmail.com>
2020-08-10 20:27:12 +0100

    Updates

--- precache-manifest.b89d7b86838653ac508392f7f6962fa4.js
+++ precache-manifest.b89d7b86838653ac508392f7f6962fa4.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f2515a552d3e327575f78b2cc9dd4670",
+    "revision": "24e7e9ae531758983e9a42ff7f98f869",
     "url": "/which-zelda/index.html"
   },
   {
-    "revision": "a25c5dbe27143e0c1b13",
-    "url": "/which-zelda/static/css/main.e39d5e6d.chunk.css"
+    "revision": "0420031c397db7e0b676",
+    "url": "/which-zelda/static/css/main.ab0fb60b.chunk.css"
   },
   {
     "revision": "fd81425bcd1e4e481bb7",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/which-zelda/static/js/2.90a84ea0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a25c5dbe27143e0c1b13",
-    "url": "/which-zelda/static/js/main.a65ce8b0.chunk.js"
+    "revision": "0420031c397db7e0b676",
+    "url": "/which-zelda/static/js/main.53872f79.chunk.js"
   },
   {
     "revision": "344349d09b2e88988a02",
